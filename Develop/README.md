b9f977a8ece44ef05340587ce73d73da1fa98f96
Tristin Barnett <tristinbarnett@gmail.com>
2020-03-19 08:58:48 -0700

    added details to generate markdown

--- Develop/README.md
+++ Develop/README.md
@@ -1,3 +1,30 @@
 
-# sdf
+# dsf
+
+sdg
+
+## Table of Contents
+gsdf
+
+## Installing
+dsf
+
+## Usage
+sdf
+
+## Running the tests
+gsdgsg
+
+## Contributing
+sdfdsfadf
+
+## Author
+tristinbarnett 
+
+<img src="https://avatars1.githubusercontent.com/u/59523370?v=4" width="200" height="200" />
+
+Contact Email: null
+
+## License
+sdf
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,6 +3,33 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+${data.description}
+
+## Table of Contents
+${data.TOC}
+
+## Installing
+${data.install}
+
+## Usage
+${data.usage}
+
+## Running the tests
+${data.testing}
+
+## Contributing
+${data.contributing}
+
+## Author
+${data.username} 
+
+<img src="${data.avatar}" width="200" height="200" />
+
+Contact Email: ${data.email}
+
+## License
+${data.license}
+
 `;
 }
 
